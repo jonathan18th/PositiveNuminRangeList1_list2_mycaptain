86a9eddde050d8352c409e2b37ac55e3fe9d94f5
jonathan18th <87244194+jonathan18th@users.noreply.github.com>
2021-07-15 00:28:09 +0100

    Add files via upload

---
+++ List1PositiveinRange.py
@@ -0,0 +1,4 @@
+list1 = [12, -7, 5, 64,-14]
+print("Orignal numbers in the list : ", list1)
+new_list1 = list(filter (lambda x: x >0, list1))
+print("Positive numbers in the list: ",new_list1)
---
+++ List2positivesInRange.py
@@ -0,0 +1,4 @@
+list2 = [12, 14, -95, 3]
+print("Orignal numbers in the list : ", list2)
+new_list2 = list(filter (lambda x: x >0, list2))
+print("Positive numbers in the list: ",new_list2)
